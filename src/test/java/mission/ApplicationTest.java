0e003c94c75013f3d53992e019309e08c59b9a8b
kokkumong <jeongkh2001@gmail.com>
2025-07-25 23:24:58 +0900

    #7 정규호

--- src/test/java/mission/ApplicationTest.java
+++ src/test/java/mission/ApplicationTest.java
@@ -1,15 +1,20 @@
 package mission;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import api.TestEnvironment;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class ApplicationTest extends TestEnvironment {
     @Test
+    @Timeout(10)
     void testApplication() {
+        run(List.of("예약하기", "2025-07-29", "11:00-12:00", "w","에약보기", "그만하기"));
+        assertTrue(output().contains("|2025-07-29 11:00\t|2025-07-29 12:00\t|w         |"));
     }
 
     @Override
